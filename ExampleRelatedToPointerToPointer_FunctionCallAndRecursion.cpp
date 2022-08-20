4a4d9e856691f1151789f51ddd4cb1faef91cee4
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-08-20 13:59:49 +0530

    Add files via upload

--- ExampleRelatedToPointerToPointer_FunctionCallAndRecursion.cpp
+++ ExampleRelatedToPointerToPointer_FunctionCallAndRecursion.cpp
@@ -9,7 +9,7 @@ int main()
     ptr1 = &i;//Assigning Address of variable i to pointer variable ptr1
     ptrptr1 = &ptr1;//Assigning Address of pointer variable ptr1 to Address of Pointer To Pointer's variable ptrptr1 
     ptrptr2 = ptrptr1;//Assigning Address of Pointer To Pointer's variable ptrptr1 to Address of Pointer To Pointer's variable ptrptr2 
-    ptrptr3 = ptrptr1;//Assigning Address of Pointer To Pointer's variable ptrptr2 to Address of Pointer To Pointer's variable ptrptr3
+    ptrptr3 = ptrptr2;//Assigning Address of Pointer To Pointer's variable ptrptr2 to Address of Pointer To Pointer's variable ptrptr3
     cout<< "ptrptr2(i) = " << **ptrptr2 <<endl;//We get the value of i
     cout<< "ptrptr1(i) = " << **ptrptr1 <<endl;//We get the value of i
     cout<< "ptrptr1(i) = " << **ptrptr3 <<endl;//We get the value of i
--- ExampleRelatedToPointers_FunctionCallAndRecursion.cpp
+++ ExampleRelatedToPointers_FunctionCallAndRecursion.cpp
@@ -7,7 +7,7 @@ int main()
     int *ptr1, *ptr2, *ptr3;
     ptr1 = &i;//Assigning Address of variable i to pointer variable ptr1
     ptr2 = ptr1;//Assigning Address of Pointer Variable(ptr1) to Another(ptr2)
-    ptr3 = ptr1;//Assigning Address of Pointer Variable(ptr1) to Another(ptr3)
+    ptr3 = ptr2;//Assigning Address of Pointer Variable(ptr2) to Another(ptr3)
     cout<< "ptr_2 (i) = " <<*ptr2 <<endl;//We get the value of i
     cout<< "ptr_1(i) = " << *ptr1 <<endl;//We get the value of i
     cout<< "ptr3(i) = " << *ptr3 <<endl;//We get the value of i
