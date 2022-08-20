c826a3e6cfd67ae5000f64e68cb8565f3f25a3de
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-08-20 13:56:17 +0530

    Add files via upload

--- ExampleRelatedToPointerToPointer_FunctionCallAndRecursion.cpp
+++ ExampleRelatedToPointerToPointer_FunctionCallAndRecursion.cpp
@@ -4,15 +4,14 @@ int main()
 {
     //Constant Pointer To An Integer(which is not constant)
     int i = 10;
-    int *ptr1, *ptr2;
-    int **ptrptr1 , **ptrptr2;
-    ptr1 = &i;
-    ptr2 = ptr1;
-    ptrptr1 = &ptr1;
-    ptrptr2 = ptrptr1;
-    cout<< "ptr_2 (i) = " <<*ptr2 <<endl;
-    cout<< "ptr_1(i) = " << *ptr1 <<endl;
-    cout<< "ptrptr2(i) = " << **ptrptr2 <<endl;
-    cout<< "ptrptr1(i) = " << **ptrptr1 <<endl;
+    int *ptr1;
+    int **ptrptr1 , **ptrptr2,**ptrptr3;
+    ptr1 = &i;//Assigning Address of variable i to pointer variable ptr1
+    ptrptr1 = &ptr1;//Assigning Address of pointer variable ptr1 to Address of Pointer To Pointer's variable ptrptr1 
+    ptrptr2 = ptrptr1;//Assigning Address of Pointer To Pointer's variable ptrptr1 to Address of Pointer To Pointer's variable ptrptr2 
+    ptrptr3 = ptrptr1;//Assigning Address of Pointer To Pointer's variable ptrptr2 to Address of Pointer To Pointer's variable ptrptr3
+    cout<< "ptrptr2(i) = " << **ptrptr2 <<endl;//We get the value of i
+    cout<< "ptrptr1(i) = " << **ptrptr1 <<endl;//We get the value of i
+    cout<< "ptrptr1(i) = " << **ptrptr3 <<endl;//We get the value of i
     return 0;
 }
--- ExampleRelatedToPointers_FunctionCallAndRecursion.cpp
+++ ExampleRelatedToPointers_FunctionCallAndRecursion.cpp
@@ -4,10 +4,12 @@ int main()
 {
     //Constant Pointer To An Integer(which is not constant)
     int i = 10;
-    int *ptr1, *ptr2;
-    ptr1 = &i;
-    ptr2 = ptr1;
-    cout<< "ptr_2 (i) = " <<*ptr2 <<endl;
-    cout<< "ptr_1(i) = " << *ptr1 <<endl;
+    int *ptr1, *ptr2, *ptr3;
+    ptr1 = &i;//Assigning Address of variable i to pointer variable ptr1
+    ptr2 = ptr1;//Assigning Address of Pointer Variable(ptr1) to Another(ptr2)
+    ptr3 = ptr1;//Assigning Address of Pointer Variable(ptr1) to Another(ptr3)
+    cout<< "ptr_2 (i) = " <<*ptr2 <<endl;//We get the value of i
+    cout<< "ptr_1(i) = " << *ptr1 <<endl;//We get the value of i
+    cout<< "ptr3(i) = " << *ptr3 <<endl;//We get the value of i
     return 0;
 }
